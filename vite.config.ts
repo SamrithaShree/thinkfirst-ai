ec073c647433d3b413bee95f8fedc402d28a88ea
shreya21p <021sods@gmail.com>
2026-01-04 00:04:58 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    port: 3000, 
+    port: 3000,
     host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
   },
   plugins: [react()],
   resolve: {
